4a924af286cc7faaf0e4c857ec24b00777d6d1f4
indika <indika@localhost>
2022-10-30 00:05:18 +0400

    - Integrate new template - Install bootstrap modules - Install javascript libraries

--- assets/app.js
+++ assets/app.js
@@ -4,9 +4,24 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+
+const $ = require('jquery');
+global.$ = global.jQuery = $;
+
+require('easing-js');
+import './library/bootstrap5/dist/js/bootstrap.js';
 import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
+import './library/waypoints/waypoints.min.js';
+import './library/waypoints/waypoints.min.js';
+//import './library/easing/easing.js';
+import './library/parallax/parallax.min.js';
+import './library/parallax/parallax.min.js';
+import './library/owlcarousel/owl.carousel.min.js';
+import './library/counterup/counterup.js';
+import './library/lightbox/js/lightbox.min.js';
+import './template/main.js';
+
+  
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace In\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+
+    /**
+     * @Route("/home", name="app_home")
+     */
+    public function index(): Response
+    {
+
+
+        $data = [
+            'category' => '...',
+            'promotions' => ['...', '...'],
+        ];
+
+        return $this->render('home/index.html.twig', $data);
+    }
+
+}
